703a053de3b004b3f55cb390168a1bbc2674fd3e
Thales <contato.thales@gmail.com>
2025-02-25 14:05:55 -0300

    Script para buscar no google

---
+++ googling.py
@@ -0,0 +1,14 @@
+# Este script faz uma busca no google sobre o tema escrito.
+
+#from pygooglenews import GoogleNews
+#import json
+#import time
+
+#gn = GoogleNews()
+#top = gn.top_news()
+
+#buscar_texto = print(input("Digite o que deseja buscar: "))
+
+#resultado = gn.search(buscar_texto)
+
+#print(resultado)
