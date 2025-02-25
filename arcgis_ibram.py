5bbfe5eb28f2b861bef8cd73d866414a1e32f99b
Thales <contato.thales@gmail.com>
2025-02-25 10:48:02 -0300

    Atualizando arquivo principal

---
+++ arcgis_ibram.py
@@ -0,0 +1,7 @@
+# Example root URL for ArcGIS Online
+# https://org.arcgis.com/sharing/rest
+
+# Example root URL for ArcGIS Enterprise
+# https://machine.domain.com/webadaptor/sharing/rest
+
+print("Novo Sistema para API Mineração")
